4b996929059abb86701e17371828a984e33954e1
J-M <jean-michel.perraud@csiro.au>
2017-12-21 18:27:33 +1100

    Fix date parsing - seems Pandas has nasty US date defaults lurking.

--- gr4j-calibration/iterate_over_catchments.py
+++ gr4j-calibration/iterate_over_catchments.py
@@ -7,11 +7,13 @@ import pandas as pd
 import numpy as np
 import spotpy
 import sys
+import os
 
 if(sys.platform == 'win32'):
     sys.path.append(r'C:\src\github_jm\gr4j-sg')
 else:
     sys.path.append('/home/per202/src/csiro/stash/per202/bf/gr4j-sg')
+
 
 import gr4j
 
@@ -26,23 +28,16 @@ start_month = tseries_df['month'][0]
 start_day = tseries_df['date'][0]  # date ?? anyway...
 
 tseries_len = tseries_df.shape[0] 
-rng = pd.date_range(str(start_day) + '/' + str(start_month) + '/' + str(start_year), periods=tseries_len, freq='D')
+
+## NOTE: use yyyy-mm-dd for date format. Pandas seems to use the silly US format for dd/mm/yyyy even on machines with en_AU locales. 
+rng = pd.date_range(str(start_year) + '-' + str(start_month) + '-' + str(start_day), periods=tseries_len, freq='D')
 
 # "Fun" facts: a pd.Series object cannot be at argument to pd.Series, it seems
 rainfall_data = pd.Series(tseries_df['Rain'].as_matrix(), index=rng)
 
-# Sanity check: does time stamps match what I see via excel? Nope!
-# This will need to be reconciled before doing real runs with it.
+# Sanity check: does time stamps match what I see via excel? 
 rainfall_data.head()
 rainfall_data.tail()
-# 2012-01-03    3.748100
-# 2012-01-04    0.056487
-# 2012-01-05    0.068906
-# 2012-01-06    1.125100
-# 2012-01-07    8.436600
-# But the tail end via excel is:
-# 2012	6	29	1.1251	0.98921	-9999
-# 2012	6	30	8.4366	0.94867	-9999
 
 pet_data = pd.Series(tseries_df['Etp'].as_matrix(), index=rng)
 obs_runoff_data = pd.Series(tseries_df['Qobs'].as_matrix(), index=rng)
@@ -56,10 +51,17 @@ from datetime import datetime
 run_start = datetime(1980, 1, 1)
 run_end = datetime(1999, 12, 31)
 
-cal_inputs = {}
-cal_inputs['rain'] = rainfall_data[run_start:run_end].as_matrix()
-cal_inputs['pet'] = pet_data[run_start:run_end].as_matrix()
-cal_inputs['obs'] = obs_runoff_data[run_start:run_end].as_matrix()
+cal_inputs = {
+    'rainfall' : rainfall_data[run_start:run_end].as_matrix(),
+    'pet'  : pet_data[run_start:run_end].as_matrix(),
+    # 'obs'  : obs_runoff_data[run_start:run_end].as_matrix(),
+    'x1':20.0,
+    'x2':0.0,
+    'x3':40.0,
+    'x4':0.5
+}
 
+runoff = gr4j.gr4j(**cal_inputs)
 
-
+library(lubridate)
+ymd('1975-01-01')
